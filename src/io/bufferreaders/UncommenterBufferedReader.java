012fef36d7300c3ec452ee41165892c90cdca9f1
javier <javier@martin-FIL>
2013-08-13 17:47:56 -0300

    Modified a bug in UncommenterBufferedReader

--- src/io/bufferreaders/UncommenterBufferedReader.java
+++ src/io/bufferreaders/UncommenterBufferedReader.java
@@ -9,26 +9,44 @@ import removecomments.RemoveComments;
 
 public class UncommenterBufferedReader extends BufferedReader {
 
+	private RemoveComments remover; 
+	
 	public UncommenterBufferedReader(Reader in) {
+		
 		super(in);
+		
+		remover = new RemoveComments();
+		
 	}
 
 	@Override
 	public String readLine() throws IOException {
 		
+		String currentLine = null; 
+				
+		while ((currentLine = this.getNextLine()) != null && currentLine.equals(""));
+				
+		return currentLine;
+			
+	}
+	
+	
+	public String getNextLine() throws IOException {
+		
 		String currentLine = super.readLine();
-
+		
 		if (currentLine!=null) {
 		
 			StringBuffer sb = new StringBuffer(currentLine);
 		
-			String result = (new RemoveComments()).uncomment(new ByteArrayInputStream(sb.toString().getBytes()));
+			String result = remover.uncomment(new ByteArrayInputStream(sb.toString().getBytes()));
 		
 			return result.trim();
 			
 		} else {
 			
 			return null;
+			
 		}
 		
 	}
