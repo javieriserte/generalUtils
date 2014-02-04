46eab15701757e2dddab65aa178b55037e082b0e
Javier Iserte <jiserte@unq.edu.ar>
2014-02-04 12:11:05 -0300

    Changes in CompareRanks class

--- src/math/ranks/CompareRanks.java
+++ src/math/ranks/CompareRanks.java
@@ -5,75 +5,144 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-import cmdGA.NoOption;
-import cmdGA.Parser;
-import cmdGA.SingleOption;
 import cmdGA.exceptions.IncorrectParameterTypeException;
-import cmdGA.parameterType.InFileParameter;
-import cmdGA.parameterType.PrintStreamParameter;
+import cmdGA2.CommandLine;
+import cmdGA2.NoArgumentOption;
+import cmdGA2.OptionsFactory;
+import cmdGA2.SingleArgumentOption;
+import cmdGA2.returnvalues.InfileValue;
 
+/**
+ * Compares the order of two rankings.<br>
+ * Comparison can be made by three methods: 
+ * <ol>
+ * <li> Spearman ranking correlation score. (default)
+ * <li> Pearson coefficient.
+ * <li> Absolute difference in rank positions. 
+ * </ol>
+ * 
+ * 
+ * @author Javier Iserte
+ * 
+ */
 public class CompareRanks {
 	
 	public static void main(String[] arg) throws IncorrectParameterTypeException {
 		
-		Parser parser = new Parser();
+		///////////////////////////
+		// Creates command line 
+		CommandLine commandLine = new CommandLine();
 		
+		////////////////////////////
+		// Add options to the command line
+		SingleArgumentOption<File> rank1FileOpt = new SingleArgumentOption<File>(commandLine, "--rank1", new InfileValue() , null);
+		SingleArgumentOption<File> rank2FileOpt = new SingleArgumentOption<File>(commandLine, "--rank2", new InfileValue() , null);
+		SingleArgumentOption<PrintStream> outOpt = OptionsFactory.createBasicPrintStreamArgument(commandLine);
+		NoArgumentOption spearmamOpt = new NoArgumentOption(commandLine, "--spearman");
+		NoArgumentOption pearsonOpt = new NoArgumentOption(commandLine, "--pearson");
+		NoArgumentOption diffOpt = new NoArgumentOption(commandLine, "--diff");
 		
-		SingleOption rank1fileOpt = new SingleOption(parser, null, "-rank1", InFileParameter.getParameter());
+		/////////////////////////////
+		// Parse command line
+		commandLine.readAndExitOnError(arg);
 		
-		SingleOption rank2fileOpt = new SingleOption(parser, null, "-rank2", InFileParameter.getParameter());
-		
-		SingleOption outOpt = new SingleOption(parser, System.out, "-outfile", PrintStreamParameter.getParameter());
-		
-		NoOption srccOpt = new NoOption(parser, "-Spearman");
-		
-		NoOption pearOpt = new NoOption(parser, "-Pearson");
-		
-		parser.parseEx(arg);
-		
-		RankingComparator rc = null;
-		
-		if (srccOpt.isPresent()) {
-			
-			rc = new SpearmanCorrelation();
-			
-		} else 
-		if (pearOpt.isPresent()) {
-			
-			rc = new PearsonComparator();
-			
-		} else{
-			
-			rc = new DifferenceCorrelation();
-			
-		}
-		
-		PrintStream out = (PrintStream) outOpt.getValue();
-		
-		if ( rank1fileOpt.isPresent() && rank2fileOpt.isPresent()) {
-			
-			File r1file = (File) rank1fileOpt.getValue();
-			
-			File r2file = (File) rank2fileOpt.getValue();
-			
-			Map<Integer, Integer> rank1 = readRank(r1file);
-			
-			Map<Integer,Integer> rank2 = readRank(r2file);
-
-			out.println(rc.compare(rank1, rank2));
-			
-		} else {
+		/////////////////////////////
+		// Validate command line
+		if (! (rank1FileOpt.isPresent() && rank2FileOpt.isPresent())) {
 			
 			System.err.println("-rank1 and -rank2 options are mandatory.");
 			
+			System.exit(0);
+			
 		}
+		
+		///////////////////////////////
+		// Create output print stream
+		PrintStream out = outOpt.getValue();
+		
+		//////////////////////////////
+		// Create a Ranking Comparator
+		RankingComparator rc = null;
+		rc = getRankingComparator(spearmamOpt, pearsonOpt, diffOpt);
+
+		///////////////////////////////
+		// Read input files
+		File rank1File = rank1FileOpt.getValue();
+		File rank2File = rank2FileOpt.getValue();
+		Map<Integer, Integer> rank1 = readRank(rank1File);
+		Map<Integer,Integer> rank2 = readRank(rank2File);
+		
+		//////////////////////////////
+		// Check the both ranks are the same size.
+		if (rank1.size() != rank2.size()) {
+			System.err.println("Ranks have different sizes");
+			System.exit(1);
+		}
+		//////////////////////////////
+		// Check that both ranks use the same labels
+		
+		Iterator<Integer> it = rank1.keySet().iterator();
+		boolean found=true;
+		while(it.hasNext() && found) {
+			Integer testedKey = it.next();
+			found = rank2.containsKey(testedKey);
+		}
+		if (!found) {
+			System.err.println("Ranks have different labels");
+			System.exit(1);
+		}
+
+		///////////////////////////////
+		// Compare ranks and prints output
+		out.println(rc.compare(rank1, rank2));
 		
 	}
 
+	/**
+	 * Get the corresponding ranking comparator from the given command line 
+	 * arguments
+	 * @param spearmamOpt
+	 * @param pearsonOpt
+	 * @param diffOpt
+	 * @param rc
+	 * @return
+	 */
+	private static RankingComparator getRankingComparator(
+			NoArgumentOption spearmamOpt, 
+			NoArgumentOption pearsonOpt,
+			NoArgumentOption diffOpt) {
+
+		RankingComparator rc;
+		
+		if (pearsonOpt.isPresent()) {
+			
+			rc = new PearsonComparator();
+			
+		} else
+		if (diffOpt.isPresent()){
+			
+			rc = new DifferenceCorrelation();
+			
+		} else {
+			
+			rc = new SpearmanCorrelation();
+			
+		}  
+		return rc;
+	}
+
+	/**
+	 * Reads a rank from a file. The keys in the return map are labels, 
+	 * the values are the positions in the ranking.
+	 * @param file
+	 * @return
+	 */
 	private static Map<Integer, Integer> readRank(File file) {
 		
 		try {
@@ -98,15 +167,45 @@ public class CompareRanks {
 			return map;
 			
 		} catch (FileNotFoundException e) {
-			System.err.println("There was an erro trying to open a file:" + e.getMessage());
+			System.err.println("There was an error trying to open a file:" + e.getMessage());
 			System.exit(1);
 		} catch (IOException e) {
-			System.err.println("There was an erro trying to open a file:" + e.getMessage());
+			System.err.println("There was an error trying to open a file:" + e.getMessage());
 			System.exit(1);
 		} 
 		
 		return null;
 		
+	}
+
+	/**
+	 * Prints help into the standard error buffer.
+	 */
+	public static void printHelp() {
+		
+		InputStream helpStream = CompareRanks.class.getResourceAsStream("help");
+		
+		PrintStream ps = new PrintStream(System.err);
+		
+		byte current = -1;
+
+		try {
+			
+			while ((current = (byte) helpStream.read())!=-1) {
+			
+				ps.write(current);
+			
+			}
+		
+		ps.flush();
+		
+		ps.close();
+			
+		} catch (IOException e) {
+			
+			System.err.println("There was an error trying to print the help.");
+			
+		}
 	}
 	
 }
